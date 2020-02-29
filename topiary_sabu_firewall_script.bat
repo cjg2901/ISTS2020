388f4af209a1079519b9606e0ec6d26fb2bd71a7
CJ Gebo <46651000+cjg2901@users.noreply.github.com>
2020-02-28 20:03:35 -0500

    Update topiary_sabu_firewall_script.bat

--- topiary_sabu_firewall_script.bat
+++ topiary_sabu_firewall_script.bat
@@ -6,6 +6,9 @@ netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 netsh advfirewall firewall add rule name=”RDP” dir=out protocol=tcp localport=3389 action=allow
 netsh advfirewall firewall add rule name=”RDP” dir=in protocol=tcp localport=3389 action=allow
 
+netsh advfirewall firewall add rule name="WinRM-in" protocol=tcp localport=5985,5986 action=allow
+netsh advfirewall firewall add rule name="WinRM-out" protocol=tcp localport=5985,5986 action=allow
+
 netsh advfirewall firewall add rule name=”HTTPI” dir=in protocol=tcp localport=443,80 action=allow
 netsh advfirewall firewall add rule name=”HTTPO” dir=out protocol=tcp localport=443,80 action=allow
 
